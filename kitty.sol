ad69f748088fd97112a9655ae74f1969d40950be
TroyCassels <119761709+TroyCassels@users.noreply.github.com>
2023-06-05 20:38:43 +1000

    Update kitty.sol

--- kitty.sol
+++ kitty.sol
@@ -1,53 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-contract votingSystem{
-    
-    mapping (uint => kittyCandidate) public kittyCandidates;
-    mapping (address => kittyVoter) public voters;
-    address public kittyAdmin;
-    uint id;
-    uint public totalVoter = 0;
-    uint public totalCandidate=0;
-    
-    constructor () {
-        kittyAdmin = msg.sender;
-    }
-    
-    struct kittyVoter{
+contract VotingSystem {
+    struct Candidate {
         string name;
-        address voterAddress;
-        uint hasVoted;
-    }    
-
-    struct kittyCandidate{
-        uint kittyId;
-        string kittyName;
         uint voteCount;
     }
-
-    function registerKittyCandidate (string memory _kittyName) public {
-        require (msg.sender == kittyAdmin , "only admin can authorize this function");
-        id++;
-        kittyCandidates[id] = kittyCandidate (id, _kittyName , 0);
-        totalCandidate++;
-
+    mapping(address => bool) public votersList;
+    Candidate[] public candidatesList;
+    constructor() {
+        // Initialize the candidates list with their names and initial vote counts
+        candidatesList.push(Candidate("Candidate 1", 0));
+        candidatesList.push(Candidate("Candidate 2", 0));
+        candidatesList.push(Candidate("Candidate 3", 0));
     }
-
-    function registerKittyVoter (address _voterAddress, string memory _name) public {
-        require (msg.sender == kittyAdmin , "only admin can authorize this function");
-        kittyVoter memory voter;
-        voter.name =_name;
-        voter.hasVoted = 0; // 0 = has not voted & 1 = has voted
-        voters[_voterAddress] = voter;
-        totalVoter++;
+    function hasVoted(address voter) internal view returns (bool) {
+        // Check if the voter has already voted
+        return votersList[voter];
     }
-
-    // function voteTo(string memory _kittyName) public {
-    //     require (voters[voter] =)
-    // }
-
+    function isValidCandidate(string memory candidateName) internal view returns (bool) {
+        // Check if the candidate name is valid by iterating through the candidates list
+        for (uint i = 0; i < candidatesList.length; i++) {
+            if (keccak256(bytes(candidatesList[i].name)) == keccak256(bytes(candidateName))) {
+                return true;
+            }
+        }
+        return false;
+    }
+    function castVote(string memory candidateName) public {
+        address voter = msg.sender;
+        require(!hasVoted(voter), "Voter has already voted");
+        require(isValidCandidate(candidateName), "Invalid candidate name");
+        // Increment the vote count for the selected candidate
+        for (uint i = 0; i < candidatesList.length; i++) {
+            if (keccak256(bytes(candidatesList[i].name)) == keccak256(bytes(candidateName))) {
+                candidatesList[i].voteCount++;
+                break;
+            }
+        }
+        // Mark the voter as voted
+        votersList[voter] = true;
+    }
+    function winner() public view returns (string memory winnerName, uint winnerVotes) {
+        uint maxVotes = 0;
+        // Find the candidate with the highest vote count
+        for (uint i = 0; i < candidatesList.length; i++) {
+            if (candidatesList[i].voteCount > maxVotes) {
+                maxVotes = candidatesList[i].voteCount;
+                winnerName = candidatesList[i].name;
+                winnerVotes = candidatesList[i].voteCount;
+            }
+        }
+        // Return the name and vote count of the winner
+        return (winnerName, winnerVotes);
+    }
 }
+
+
+
 
 
 
